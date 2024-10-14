f43e50beed86026ebcb6d9d6b1afe2845b3d77f5
Reece Harris <reece@codegalaxy.co.uk>
2024-10-14 16:03:33 +0100

    Cap-typo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 
-	import Header from '$components/Header.svelte';
-	import Footer from '$components/Footer.svelte';
+	import Header from '$components/header.svelte';
+	import Footer from '$components/footer.svelte';
 	import '../app.css';
-	
+
 </script>
 
 <div class="bg-background flex flex-col min-h-screen w-screen max-w-screen">
