0ecd2f0436dd8ba80050a55ac01f395b08052702
Reece Harris <reece@codegalaxy.co.uk>
2024-10-17 10:52:53 +0100

    Refactor pricing component links and update free trial text

--- src/components/pricing.svelte
+++ src/components/pricing.svelte
@@ -13,7 +13,7 @@
             class="{period === 'yearly'
                 ? 'hover:font-medium text-sm font-thin'
                 : 'font-bold'} ml-0.5 relative w-1/2 border rounded-sm py-2 whitespace-nowrap focus:outline-none sm:w-auto sm:px-8 border-transparent text-white"
-            >Monthly billing
+            >Monthly Billing
         </button>
         <div class="h-6 border-r border-white/[.6]"></div>
         <button
@@ -22,7 +22,7 @@
             class="{period === 'monthly'
                 ? 'hover:font-medium text-sm font-thin'
                 : 'font-bold'} ml-0.5 relative w-1/2 border rounded-sm py-2 whitespace-nowrap focus:outline-none sm:w-auto sm:px-8 border-transparent text-white"
-            >Yearly billing
+            >Yearly Billing
         </button>
     </div>
     <div
@@ -47,21 +47,25 @@
                             >
                         {/if}
                     </div>
-                    <p class="mt-2 text-base text-white leading-tight">
+                    <p class="mt-2 text-base text-white/[.8] leading-tight">
                         {plan.description}
                     </p>
-                    <p class="mt-8">
+                    <p class="mt-6">
                         <span
                             class="text-4xl font-bold text-white tracking-tighter"
                         >
-                            $
-                            {period === "monthly"
-                                ? plan.price
-                                : plan.price * 12 * 0.95}
+                            £ {period === "monthly" ? plan.price : (plan.price * 12 * 0.95).toFixed(0)}
                         </span>
-                        <span class="text-base font-medium text-white/[.7]">
-                            /{period === "monthly" ? "mo" : "yr"}
+                        <span class="text-base font-medium text-white/[.7] ml-2">
+                            / {period === "monthly" ? "mo" : "yr"}
                         </span>
+                        {#if period == "yearly"}
+                        <div  class="text-lg font-medium text-white/[.7] tracking-tighter">
+                            <span>or pay</span>
+                            <span>£{(plan.price * 24 * 0.80).toFixed(0)}</span>
+                            <span>if you pay for 2 years.</span>
+                        </div>
+                        {/if}
                     </p>
                     {#if plan.freeTrial}
                         <a
@@ -83,62 +87,68 @@
                     <h3
                         class="text-sm font-bold text-white tracking-wide uppercase"
                     >
-                        What's included
+                        {plan.featuresHeader}
                     </h3>
                     <ul role="list" class="mt-4 space-y-3">
                         {#each plan.features as feature}
+
+                            {#if typeof feature === "string"}
+                                <li class="pt-1 min-h-[24px]">
+                                    <span>{feature}</span>
+                                </li>
+                            {:else}
+
                             <li class="flex space-x-3">
-                                {#if feature.isIncluded}
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        class="flex-shrink-0 h-5 w-5 text-green-400"
-                                        width="24"
-                                        height="24"
-                                        viewBox="0 0 24 24"
-                                        stroke-width="2"
-                                        stroke="currentColor"
-                                        fill="none"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        aria-hidden="true"
-                                    >
-                                        <path
-                                            stroke="none"
-                                            d="M0 0h24v24H0z"
+                                {#if !feature.isAddOn}
+                                    {#if feature.isIncluded}
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            class="flex-shrink-0 h-5 w-5 text-green-400/[.7]"
+                                            width="24"
+                                            height="24"
+                                            viewBox="0 0 24 24"
+                                            stroke-width="2"
+                                            stroke="currentColor"
                                             fill="none"
-                                        ></path>
-                                        <path d="M5 12l5 5l10 -10"></path>
-                                    </svg>
-                                {:else}
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        class="flex-shrink-0 h-5 w-5 text-red-400"
-                                        width="24"
-                                        height="24"
-                                        viewBox="0 0 24 24"
-                                        stroke-width="2"
-                                        stroke="currentColor"
-                                        fill="none"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        aria-hidden="true"
-                                    >
-                                        <line x1="18" y1="6" x2="6" y2="18"
-                                        ></line>
-                                        <line x1="6" y1="6" x2="18" y2="18"
-                                        ></line>
-                                    </svg>
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            aria-hidden="true"
+                                        >
+                                            <path
+                                                stroke="none"
+                                                d="M0 0h24v24H0z"
+                                                fill="none"
+                                            ></path>
+                                            <path d="M5 12l5 5l10 -10"></path>
+                                        </svg>
+                                    {:else}
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            class="flex-shrink-0 h-5 w-5 text-red-400/[.7]"
+                                            width="24"
+                                            height="24"
+                                            viewBox="0 0 24 24"
+                                            stroke-width="2"
+                                            stroke="currentColor"
+                                            fill="none"
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            aria-hidden="true"
+                                        >
+                                            <line x1="18" y1="6" x2="6" y2="18"
+                                            ></line>
+                                            <line x1="6" y1="6" x2="18" y2="18"
+                                            ></line>
+                                        </svg>
+                                    {/if}
                                 {/if}
-                                {#if feature.isHighlighted}
-                                    <span class="text-base text-white"
-                                        >{feature.name}</span
-                                    >
-                                {:else}
-                                    <span class="text-base text-white/[.7]"
-                                        >{feature.name}</span
-                                    >
-                                {/if}
+                                <span class="text-base {feature.isHighlighted ? 'text-white' : 'text-white/[.7]'}">
+                                    {feature.name}
+                                </span>
                             </li>
+
+                            {/if}
+
                         {/each}
                     </ul>
                 </div>
@@ -153,6 +163,15 @@
         </p>
         <p>
             {data["dynamic-pricing-disclaimer"]}
+        </p>
+        <p>
+            {data["upgrade-disclaimer"]}
+        </p>
+        <p>
+            {data["trail-disclaimer"]}
+        </p>
+        <p>
+            Read our full <a href="/" class="text-white underline">Terms of Service</a> and <a href="/" class="text-white underline">Privacy Policy</a>.
         </p>
     </div>
 </div>
--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,108 +3,196 @@ export async function load() {
 	return {
 		components: {
             pricing: {
-                'discount-disclaimer': '* %5 off when you pay yearly, %10 off when you pay for 2+ years upfront !',
+                'discount-disclaimer': '* %5 off when you pay yearly, %20 off when you pay for 2+ years upfront !',
                 'dynamic-pricing-disclaimer': '* We use dynamic pricing based on several factors: supply and demand, data-driven insights, regular market analysis, and usage. We are constantly improving our pricing model to provide the best value for our customers. ',
+                'upgrade-disclaimer': '* You can upgrade or downgrade your plan at any time. The changes will take effect at the end of the current billing cycle.',
+                'trail-disclaimer': '* Free trials are available for 7 days. You can cancel your subscription at any time during the trial period. After the trial period ends, you will be charged for the selected plan.',
                 'plans': [
                     {
                         'name': 'Basic',
-                        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, voluptate?',
-                        'price': 10,
+                        'description': 'Perfect for individuals and small teams, who want to get started with a simple service.',
+                        'price': 15,
                         'freeTrial': true,
+                        'featuresHeader': 'Highlighted features:',
                         'features': [
                             {
-                                'name': 'Feature 1',
+                                'name': '2 Services',
                                 'description': 'This is a description of Feature 1.',
                                 'isHighlighted': false,
-                                'isIncluded': true
+                                'isIncluded': true,
+                                'isAddOn': false
                             },
                             {
-                                'name': 'Feature 2',
+                                'name': '4 Team Members',
                                 'description': 'This is a description of Feature 2.',
                                 'isHighlighted': false,
-                                'isIncluded': true
+                                'isIncluded': true,
+                                'isAddOn': false
                             },
                             {
-                                'name': 'Feature 3',
+                                'name': 'Complete Customisation',
                                 'description': 'This is a description of Feature 3.',
                                 'isHighlighted': false,
-                                'isIncluded': true
+                                'isIncluded': true,
+                                'isAddOn': false
                             },
+                            {
+                                'name': 'Template Reports',
+                                'description': 'This is a description of Feature 3.',
+                                'isHighlighted': false,
+                                'isIncluded': true,
+                                'isAddOn': false
+                            },
+                            {
+                                'name': 'Automation Tools',
+                                'description': 'This is a description of Feature 3.',
+                                'isHighlighted': false,
+                                'isIncluded': true,
+                                'isAddOn': false
+                            },
+                            {
+                                'name': 'Migration Tools',
+                                'description': 'This is a description of Feature 3.',
+                                'isHighlighted': false,
+                                'isIncluded': true,
+                                'isAddOn': false
+                            },
+                            {
+                                'name': 'Limited API Access',
+                                'description': 'This is a description of Feature 3.',
+                                'isHighlighted': false,
+                                'isIncluded': true,
+                                'isAddOn': false
+                            },
+                            'Addons: ',
+                            {
+                                'name': '10% Commission on Referrals.',
+                                'description': 'This is a description of Feature 2.',
+                                'isHighlighted': false,
+                                'isIncluded': true,
+                                'isAddOn': true
+                            },
+                            {
+                                'name': '20% Discount on Extra Services.',
+                                'description': 'This is a description of Feature 2.',
+                                'isHighlighted': false,
+                                'isIncluded': true,
+                                'isAddOn': true
+                            }
                         ]
                     },
                     {
                         'name': 'Pro',
-                        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, voluptate?',
-                        'price': 20,
+                        'description': 'Great for growing businesses, who want to get more customers and increase their revenue.',
+                        'price': 25,
                         'freeTrial': true,
+                        'featuresHeader': 'Everything in Basic, plus:',
                         'features': [
                             {
-                                'name': 'Feature 1',
+                                'name': '4 Services',
                                 'description': 'This is a description of Feature 1.',
                                 'isHighlighted': false,
-                                'isIncluded': true
+                                'isIncluded': true,
+                                'isAddOn': false
                             },
                             {
-                                'name': 'Feature 2',
+                                'name': '6 Team Members',
                                 'description': 'This is a description of Feature 2.',
-                                'isHighlighted': true,
-                                'isIncluded': true
+                                'isHighlighted': false,
+                                'isIncluded': true,
+                                'isAddOn': false
                             },
                             {
-                                'name': 'Feature 3',
+                                'name': 'AI-Powered Insights',
                                 'description': 'This is a description of Feature 3.',
-                                'isHighlighted': true,
-                                'isIncluded': true
+                                'isHighlighted': false,
+                                'isIncluded': true,
+                                'isAddOn': false
                             },
                             {
-                                'name': 'Feature 4',
-                                'description': 'This is a description of Feature 4.',
+                                'name': 'Custom Reports',
+                                'description': 'This is a description of Feature 3.',
                                 'isHighlighted': false,
-                                'isIncluded': false
+                                'isIncluded': true,
+                                'isAddOn': false
                             },
                             {
-                                'name': 'Feature 5',
-                                'description': 'This is a description of Feature 5.',
+                                'name': 'Rate limited Full API Access',
+                                'description': 'This is a description of Feature 3.',
                                 'isHighlighted': false,
-                                'isIncluded': false
+                                'isIncluded': true,
+                                'isAddOn': false
                             },
+                            {
+                                'name': 'Slack & Email Notifications',
+                                'description': 'This is a description of Feature 3.',
+                                'isHighlighted': false,
+                                'isIncluded': true,
+                                'isAddOn': false
+                            },
+                            {
+                                'name': 'Email Support',
+                                'description': 'This is a description of Feature 3.',
+                                'isHighlighted': false,
+                                'isIncluded': true,
+                                'isAddOn': false
+                            },
+                            'Addons: ',
+                            {
+                                'name': '15% Commission on Referrals.',
+                                'description': 'This is a description of Feature 2.',
+                                'isHighlighted': false,
+                                'isIncluded': true,
+                                'isAddOn': true
+                            },
+                            {
+                                'name': '50% Discount on Extra Services.',
+                                'description': 'This is a description of Feature 2.',
+                                'isHighlighted': false,
+                                'isIncluded': true,
+                                'isAddOn': true
+                            }
                         ]
                     },
                     {
                         'name': 'Enterprise',
-                        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, voluptate?',
-                        'price': 30,
+                        'description': 'Best for large businesses, who want to get the most out of our service and support.',
+                        'price': 100,
                         'freeTrial': false,
+                        'featuresHeader': 'Everything in Pro, plus:',
                         'features': [
                             {
-                                'name': 'Feature 1',
+                                'name': '6 Services',
                                 'description': 'This is a description of Feature 1.',
                                 'isHighlighted': false,
-                                'isIncluded': true
+                                'isIncluded': true,
+                                'isAddOn': false
                             },
                             {
-                                'name': 'Feature 2',
+                                'name': '20 Team Members',
                                 'description': 'This is a description of Feature 2.',
                                 'isHighlighted': false,
-                                'isIncluded': true
+                                'isIncluded': true,
+                                'isAddOn': false
                             },
                             {
-                                'name': 'Feature 3',
-                                'description': 'This is a description of Feature 3.',
+                                'name': 'Full API Access',
+                                'description': 'This is a description of Feature 2.',
                                 'isHighlighted': false,
-                                'isIncluded': true
+                                'isIncluded': true,
+                                'isAddOn': false
                             },
+                            '',
+                            '',
+                            '',
+                            '',
+                            'Addons: ',
                             {
-                                'name': 'Feature 4',
-                                'description': 'This is a description of Feature 4.',
+                                'name': '70% Discount on Extra Services.',
+                                'description': 'This is a description of Feature 2.',
                                 'isHighlighted': false,
-                                'isIncluded': true
-                            },
-                            {
-                                'name': 'Feature 5',
-                                'description': 'This is a description of Feature 5.',
-                                'isHighlighted': false,
-                                'isIncluded': true
+                                'isIncluded': true,
+                                'isAddOn': true
                             },
                         ]
                     }
